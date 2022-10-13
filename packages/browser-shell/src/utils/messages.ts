f7b3936c5a785f88d2b1101313f5c0f68993860a
TestLearnReact <socialdev@gmx.de>
2022-10-14 00:21:15 +0200

    add prettier eslint

--- packages/browser-shell/src/utils/messages.ts
+++ packages/browser-shell/src/utils/messages.ts
@@ -1,25 +1,29 @@
-import getMessage from "@workspace/message-system";
-import { ContentScriptComponent } from "../contentScripts/types";
-import type { InPageUIComponentShowState } from "@ui/common";
+import getMessage from '@workspace/message-system';
+import { ContentScriptComponent } from '../contentScripts/types';
+import type { InPageUIComponentShowState } from '@ui/common';
 
 /** extension messages */
 export const [
   ms_sendInjectScript,
   ms_injectScriptStream,
   ms_waitForInjectScript,
-] = getMessage<{ filename: ContentScriptComponent }>("INJECT_SCRIPT");
+] = getMessage<{
+  filename: ContentScriptComponent;
+}>('INJECT_SCRIPT');
 
 export const [
   ms_sendComponentInit,
   ms_componentInitStream,
   ms_waitForComponentInit,
-] = getMessage<{ component: ContentScriptComponent }>("COMPONENT_INIT");
+] = getMessage<{
+  component: ContentScriptComponent;
+}>('COMPONENT_INIT');
 
 export const [
   ms_sendComponentDestroy,
   ms_componentDestroyStream,
   ms_waitForComponentDestroy,
-] = getMessage<{ component: ContentScriptComponent }>("COMPONENT_DESTROY");
+] = getMessage<{ component: ContentScriptComponent }>('COMPONENT_DESTROY');
 
 /**
  *
@@ -28,15 +32,15 @@ export const [
   ms_sendInPageUiState,
   ms_inPageUiStateStream,
   ms_waitForInPageUiStateStream,
-] = getMessage<InPageUIComponentShowState & { theme?: "dark" | "light" }>(
-  "INPAGE_UI_STATE"
+] = getMessage<InPageUIComponentShowState & { theme?: 'dark' | 'light' }>(
+  'INPAGE_UI_STATE',
 );
 
 export const [
   ms_sendSharedStateSettings,
   ms_sharedStateSettingsStream,
   ms_waitForSharedStateSettings,
-] = getMessage<{ theme: "dark" | "light" }>("SHARED_STATE_SETTINGS");
+] = getMessage<{ theme: 'dark' | 'light' }>('SHARED_STATE_SETTINGS');
 
 /**
  * development
@@ -45,4 +49,4 @@ export const [
   ms_sendExtensionReload,
   ms_extensionReloadStream,
   ms_waitForExtensionReload,
-] = getMessage<{}>("EXTENSION_RELOAD");
+] = getMessage<{}>('EXTENSION_RELOAD');
